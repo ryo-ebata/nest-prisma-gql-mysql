928099b23a86a0d1b63a6379de84a3fb3f8268cb
ryo-ebata <r.ebata@hajimari.inc>
2022-09-25 17:34:51 +0900

    first

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostResolver } from './posts/posts.resolver';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, PrismaService, PostResolver],
+})
+export class AppModule {}
